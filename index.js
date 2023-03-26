28af9844004f6b071daf13535654d86a0f986e51
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-03-26 14:30:22 -0700

    Returns drivers and multiplies fares.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (scuberNames) => scuberNames.slice(0, 2);
+const returnLastTwoDrivers = (scuberNames) => scuberNames.slice(scuberNames.length - 2, scuberNames.length);
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer){
+    return (fare) => integer * fare;
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(scuberDrivers, returnDrivers){
+    return returnDrivers(scuberDrivers);
+}
